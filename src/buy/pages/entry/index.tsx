5e5bcb7b1ad0554c146acf0d60f70a020ad4c4cd
yiran_aihuishou <yiran.sun@aihuishou.com>
2019-11-06 17:25:46 +0800

    s1-5:

--- src/buy/pages/entry/index.tsx
+++ src/buy/pages/entry/index.tsx
@@ -1,11 +1,38 @@
 import React, { useContext } from "react";
 import "./index.less";
 import { EntryPageContext, IEntryPageContext } from "./context";
+import { FormWrapper } from "./components/formWrapper";
+import { Input } from "antd";
 
 export default function EntryPage() {
   const entryPageContext = useContext(EntryPageContext);
   const { entryPageContextValue } = entryPageContext as IEntryPageContext;
   const { list } = entryPageContextValue;
   console.log(list);
-  return <div className="test-page">{123}</div>;
+  const formConfig = [
+    {
+      id: "haha",
+      rules: [
+        {
+          type: "email",
+          error: "hehe"
+        }
+      ],
+      renderFormEle: () => <Input />
+    },
+    {
+      renderFormEle: () => <button>submit</button>
+    }
+  ];
+
+  function onSubmitHandler(values: any) {
+    console.log(values);
+  }
+
+  return (
+    <div className="test-page">
+      // @ts-ignore
+      <FormWrapper formConfig={formConfig} onSubmit={onSubmitHandler} />;
+    </div>
+  );
 }
