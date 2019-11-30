640e6d8d92d894faa5f0e95bf2282b447fdc9bdf
yiranzi <570932296@qq.com>
2019-11-30 09:26:19 +0800

    修复mac端报错

--- src/buy/pages/focus/index.tsx
+++ src/buy/pages/focus/index.tsx
@@ -4,11 +4,8 @@ import { Switch, Route } from "react-router-dom";
 
 export function FocusRouter(props: any) {
   const { path } = props.match;
-  return (
-    <Switch>
-      {routerConfig.map(({ path: relativePath, ...others }) => {
-        return <Route path={path + relativePath} {...others} />;
-      })}
-    </Switch>
-  );
+  const dom = routerConfig.map(({ path: relativePath, ...others }) => {
+    return <Route path={path + relativePath} {...others} />;
+  });
+  return <Switch>{dom}</Switch>;
 }
--- src/buy/share/routerConfig.tsx
+++ src/buy/share/routerConfig.tsx
@@ -1,23 +1,4 @@
-import BuyHome from "../pages/home/buy-home";
-import OrderRouter from "../pages/order/orderRouter";
-import ProductList from "../pages/productList";
-import { productListSsrRule } from "../pages/productList/ssr";
-import ProductDetail from "../pages/detail";
-import UptradeProtect from "../pages/statics/uptrade-protect";
-import UptradePolicy from "../pages/statics/policy";
-import Home from "../pages/home";
 import React from "react";
-import { getLocationUrl, getProductListPath } from "../common/utils/util";
-import { detailSsrRule } from "../pages/detail/ssr";
-import { ourHomeSsrRule } from "../pages/home/ssr";
-import BuyCheckOrder from "../pages/checkOrder/routers";
-import DetailPreviewWrapper from "../pages/detail/detailPreviewWrapper";
-import PersonalLogin from "../pages/personal/pages/login";
-import AccountPage from "../pages/personal/pages/account";
-import UserRegister from "../pages/personal/pages/register";
-import UserRegisterEmail from "../pages/personal/pages/registerEmail";
-import UserForgetPassword from "../pages/personal/pages/userForgetPassword";
-import UserResetPassword from "../pages/personal/pages/userResetPassword";
 import { FocusRouter } from "../pages/focus";
 
 export const routerConfig = [
