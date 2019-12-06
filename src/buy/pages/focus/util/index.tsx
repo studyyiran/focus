84f60634f5e3e1177255d2c419a3a2df8ec3b4fd
yiran_aihuishou <yiran.sun@aihuishou.com>
2019-12-06 12:25:17 +0800

    添加分类函数注释

--- src/buy/pages/focus/util/index.tsx
+++ src/buy/pages/focus/util/index.tsx
@@ -2,20 +2,21 @@ import moment from "moment";
 
 export function todayPageFilter(data: any) {
   const jsonWithFilterData = {
-    review: [],
-    plane: [],
-    delay: [],
-    tomorrow: [],
+    plane: [],// 今日
+    review: [],// 今日复习
+    delay: [],// delay
+    tomorrow: []// 明日
   } as any;
   data.forEach((item: any) => {
     const { tag } = item;
     let isToday = false;
     if (item && item.planStartTime) {
+      // 开始时间是今天,那就是今日
       isToday = moment(item.planStartTime).isSame(moment(), "day");
     }
     if (!isToday) {
-      // 如果是明天的
-      if (moment(item.planStartTime).isSame(moment().add(1, 'days'), "day")) {
+      if (moment(item.planStartTime).isSame(moment().add(1, "days"), "day")) {
+        // 如果是明天的
         jsonWithFilterData.tomorrow.push(item);
       } else {
         // 如果已经不是当日的.扔到delay
@@ -35,11 +36,11 @@ export function todayPageFilter(data: any) {
 }
 
 export function decoratorTomorrow(data: any) {
-  return { timeType: 'tomorrow', ...data };
+  return { timeType: "tomorrow", ...data };
 }
 
 export function decoratorToday(data: any) {
-  return { timeType: 'today', ...data };
+  return { timeType: "today", ...data };
 }
 
 export function decoratorFinish(data: any) {
