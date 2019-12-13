6d2fcd4bc9c46736be9125f112b4722ec5521cdc
yiran_aihuishou <yiran.sun@aihuishou.com>
2019-12-13 11:41:53 +0800

    提交新增tag

--- src/buy/pages/focus/pages/history/components/filterPart/index.tsx
+++ src/buy/pages/focus/pages/history/components/filterPart/index.tsx
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { Select } from "antd";
 import { tagArr } from "../../../../components/newTodoModal";
 import { IMyFocusContext, MyFocusContext } from "../../../../context";
+import "./index.less";
 
 const { Option } = Select;
 export function FilterPart() {
@@ -14,15 +15,41 @@ export function FilterPart() {
     });
   }
   return (
-    <div>
-      <section>
-        <h3>Tag Part</h3>
-        <Select defaultValue={tagArr[0].value} onChange={onChangeSelectHandler}>
-          {tagArr.map(item => {
-            return <Option value={item.value}>{item.name}</Option>;
-          })}
-        </Select>
-      </section>
-    </div>
+    <ul className="selector-list">
+      <li>
+        <section>
+          <h3>Tag Part</h3>
+          <Select
+            defaultValue={tagArr[0].value}
+            onChange={onChangeSelectHandler}
+          >
+            {tagArr.map(item => {
+              return <Option value={item.value}>{item.name}</Option>;
+            })}
+          </Select>
+        </section>
+      </li>
+      <li>
+        <section>
+          <h3>Time Part</h3>
+          <Select
+            defaultValue={tagArr[0].value}
+            onChange={onChangeSelectHandler}
+          >
+            {tagArr.map(item => {
+              return <Option value={item.value}>{item.name}</Option>;
+            })}
+          </Select>
+          <Select
+            defaultValue={tagArr[0].value}
+            onChange={onChangeSelectHandler}
+          >
+            {tagArr.map(item => {
+              return <Option value={item.value}>{item.name}</Option>;
+            })}
+          </Select>
+        </section>
+      </li>
+    </ul>
   );
 }
