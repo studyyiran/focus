0e56518e5a7f68a1a8d93f03135cafcaff8fee74
yiranzi <570932296@qq.com>
2020-04-27 12:23:22 +0800

    新增seasonLine组件

---
+++ src/buy/pages/focus/pages/season/components/seasonLine/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface ISeasonLine {
+
+}
+
+export const SeasonLine: React.FC<ISeasonLine> = props => {
+  return <div className="season-line">Component</div>
+}
--- src/buy/pages/focus/pages/season/index.tsx
+++ src/buy/pages/focus/pages/season/index.tsx
@@ -1,21 +1,40 @@
 import React, { useContext, useEffect } from "react";
 import "./index.less";
 import { ISeasonContext, SeasonContext } from "./context";
+import { levelupModal } from "../targetPage/components/renderLevelUpButtons";
+import { SeasonLine } from "./components/seasonLine";
 
 export function SeasonPage() {
   // 引入context
   const seasonContext = useContext(SeasonContext);
   const {
     seasonContextValue,
-    getTestAjaxValue
+    getSeasonList,
+    startNewSeason
   } = seasonContext as ISeasonContext;
   // 从context中获取值
   const { seasonList } = seasonContextValue;
   // local发起请求
   useEffect(() => {
-    getTestAjaxValue();
-  }, [getTestAjaxValue]);
+    getSeasonList();
+  }, [getSeasonList]);
   // 渲染
   console.log(seasonList)
-  return <div className="season-page">{123}</div>;
+
+  function addNewSeasonHandler() {
+    levelupModal("name", ({name} : any) => {
+      console.log(name)
+      startNewSeason({name})
+    })
+  }
+
+  return <div className="season-page">
+    <section>
+      <h2>season List</h2>
+      {seasonList.map((props) => <SeasonLine {...props} />)}
+      <div>
+        <button onClick={addNewSeasonHandler}>add new season</button>
+      </div>
+    </section>
+  </div>;
 }
