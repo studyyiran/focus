757f4d50eb01e72305e6f876af8b362482275081
yiranzi <570932296@qq.com>
2020-04-29 19:15:50 +0800

    朝花夕拾

--- src/buy/pages/focus/pages/season/components/magicTimer/index.tsx
+++ src/buy/pages/focus/pages/season/components/magicTimer/index.tsx
@@ -1,38 +1,38 @@
 import React, { useReducer, useRef, useState } from "react";
 import "./index.less";
 import { MyTimer } from "../../../../../../common/utils/timer";
-import {seasonServer} from '../../server'
+import { seasonServer } from "../../server";
 import { levelupModal } from "../../../targetPage/components/renderLevelUpButtons";
 import { Button, Select } from "antd";
-const {Option} = Select
+const { Option } = Select;
 
 interface IMagicTimer {}
 
 const TIMERSTATUS = {
   STOP: "stop",
   DOING: "doing",
-  FINISH: "finish",
+  FINISH: "finish"
 };
 
 const reducer = (state: any, action: any) => {
-  const {type, value} = action
-  switch(type) {
-    case 'setStatus':
-      state.status = value
-      break
-    case 'setCurrentTime':
-      state.currentTime = value
-      break
+  const { type, value } = action;
+  switch (type) {
+    case "setStatus":
+      state.status = value;
+      break;
+    case "setCurrentTime":
+      state.currentTime = value;
+      break;
   }
-  return {...state}
-}
+  return { ...state };
+};
 
 export const MagicTimer: React.FC<IMagicTimer> = props => {
   const initState = {
     status: TIMERSTATUS.STOP,
-    currentTime: 25 * 1000 * 60,
-  }
-  const [state, dispatch] = useReducer(reducer, initState)
+    currentTime: 25 * 1000 * 60
+  };
+  const [state, dispatch] = useReducer(reducer, initState);
   let timerRef = useRef({} as any);
   const renderByStatus = (currentStatus: string) => {
     switch (currentStatus) {
@@ -42,9 +42,9 @@ export const MagicTimer: React.FC<IMagicTimer> = props => {
             onClick={() => {
               newTimer(state.currentTime);
               dispatch({
-                type: 'setStatus',
+                type: "setStatus",
                 value: TIMERSTATUS.DOING
-              })
+              });
             }}
           >
             start!
@@ -55,11 +55,11 @@ export const MagicTimer: React.FC<IMagicTimer> = props => {
           <button
             onClick={() => {
               if (timerRef && timerRef.current && timerRef.current.stop) {
-                timerRef.current.stop()
+                timerRef.current.stop();
                 dispatch({
-                  type: 'status',
+                  type: "status",
                   value: TIMERSTATUS.STOP
-                })
+                });
               }
             }}
           >
@@ -69,72 +69,82 @@ export const MagicTimer: React.FC<IMagicTimer> = props => {
       case TIMERSTATUS.FINISH:
         const buffConfigArr = [
           {
-            buffName: '回想',
-            content: '通过提取训练，让组块深化'
+            buffName: "回想",
+            content: "通过提取训练，让组块深化"
           },
           {
-            buffName: '课后习题',
-            content: '课后习题'
+            buffName: "课后习题",
+            content: "课后习题"
           },
           {
-            buffName: '大师级死灵书',
-            content: '通过阅读主书籍，来强化理解。扩宽理解。'
+            buffName: "大师级死灵书",
+            content: "通过阅读主书籍，来强化理解。扩宽理解。"
           },
           {
-            buffName: '聚沙成塔',
-            content: '通过将零散的内容进行整理，来在转录笔记。笔记是复习的基础'
+            buffName: "聚沙成塔",
+            content: "通过将零散的内容进行整理，来在转录笔记。笔记是复习的基础"
           },
           {
-            buffName: '食人魔',
-            content: '在吃饭的时候，去学习中文mooc'
+            buffName: "食人魔",
+            content: "在吃饭的时候，去学习中文mooc"
           },
           {
-            buffName: '骑行大法',
-            content: '在路上，收听得到课程'
+            buffName: "朝花夕拾",
+            content:
+              "项目里面遇到的问题，全部都是你提升的根本，钻石的前身，不掌握他们，你无法有任何的技术提升"
           },
           {
-            buffName: '镜像大法',
-            content: '一心两用'
+            buffName: "骑行大法",
+            content: "在路上，收听得到课程"
           },
-        ]
+          {
+            buffName: "镜像大法",
+            content: "一心两用"
+          }
+        ];
         return (
           <button
             onClick={() => {
-             // 填入时间。
+              // 填入时间。
               // 填入buff类型。
               // 进行记录。
-              levelupModal("buffName", (info: any) => {
-                seasonServer.addStudyBuffRecord({
-                  type: info.buffName,
-                  continueTime: 25 * 1000 * 60,
-                })
-              },[
-                {
-                  id: "buffName",
-                  initialValue: "",
-                  rules: [
-                    {
-                      required: true,
-                      message: "not empty"
-                    }
-                  ],
-                  renderFormEle: () => (
-                    <Select>
-                      {buffConfigArr
-                        .map(({ buffName, content }) => {
+              levelupModal(
+                "buffName",
+                (info: any) => {
+                  seasonServer.addStudyBuffRecord({
+                    type: info.buffName,
+                    continueTime: 25 * 1000 * 60
+                  });
+                },
+                [
+                  {
+                    id: "buffName",
+                    initialValue: "",
+                    rules: [
+                      {
+                        required: true,
+                        message: "not empty"
+                      }
+                    ],
+                    renderFormEle: () => (
+                      <Select>
+                        {buffConfigArr.map(({ buffName, content }) => {
                           return (
                             <Option value={buffName} key={buffName}>
                               {buffName}:{content}
                             </Option>
                           );
                         })}
-                    </Select>
-                  )
-                },
-                {
-                  renderFormEle: () => <Button htmlType="submit">submit</Button>
-                }
-              ]);
+                      </Select>
+                    )
+                  },
+                  {
+                    renderFormEle: () => (
+                      <Button htmlType="submit">submit</Button>
+                    )
+                  }
+                ]
+              );
             }}
           >
             stop!
@@ -151,9 +161,9 @@ export const MagicTimer: React.FC<IMagicTimer> = props => {
       minInterval: -1000,
       runCallBack: (times: any, timeSecond: any) => {
         dispatch({
-          type: 'setCurrentTime',
+          type: "setCurrentTime",
           value: timeSecond
-        })
+        });
       },
       finishCallBack: () => {
         // that.setState({
@@ -165,9 +175,14 @@ export const MagicTimer: React.FC<IMagicTimer> = props => {
     timerRef.current = new MyTimer(info);
     timerRef.current.start();
   };
-  return <div className="magic-timer">
-    {timerRef && timerRef.current && timerRef.current.format && timerRef.current.format(state.currentTime).join(':')}
-    {renderByStatus(state.status)}
-    {renderByStatus(TIMERSTATUS.FINISH)}
-  </div>;
+  return (
+    <div className="magic-timer">
+      {timerRef &&
+        timerRef.current &&
+        timerRef.current.format &&
+        timerRef.current.format(state.currentTime).join(":")}
+      {renderByStatus(state.status)}
+      {renderByStatus(TIMERSTATUS.FINISH)}
+    </div>
+  );
 };
